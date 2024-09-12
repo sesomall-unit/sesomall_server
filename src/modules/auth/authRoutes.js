71bd6d3b2c5b9c2f32421b32146154969e9621e5
park-youngsik <izzz110@gmail.com>
2024-09-12 15:16:05 +0900

    Add : 로그아웃 api 추가

--- src/modules/auth/authRoutes.js
+++ src/modules/auth/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { login, refreshAccessToken } = require('../auth/authController');
+const { login, logout, refreshAccessToken } = require('../auth/authController');
 
 const router = express.Router();
 
 router.post('/login', login); // POST /api/v1/auth/login 경로를 처리
+router.post('/logout', logout);  // POST /api/v1/auth/logout 경로를 처리
+
 
 // 다른 인증 관련 라우트
 router.post('/refresh-token', refreshAccessToken);
